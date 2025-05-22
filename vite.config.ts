070eb41b761f1f568d45778dde306845c5a58b45
dwesh163 <ehouarn.duriaux@protonmail.com>
2025-05-22 07:47:20 +0200

    chore: update dependencies and configure Tailwind CSS with Vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig(() => {
 	const config = {
-		plugins: [react()],
+		plugins: [tailwindcss(), react()],
+		base: '/price-calculator/',
 	};
 	return config;
 });
